9d9e739488afa770b39dfa7c6247e25f2011cc9b
Fiarr4ik <fiarr4ik@gmail.com>
2024-07-26 16:16:48 +0700

    init commit

---
+++ src/test/java/ru/fiarr4ik/restapiauth/RestApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.restapiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
